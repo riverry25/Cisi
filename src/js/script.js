608ca897ae8a999ff0f5abdf6618dad3bda5736f
Your Name <you@example.com>
2025-06-05 13:00:20 +0700

    fix

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 const navbar = document.getElementById('navbar');
 const toggle = document.getElementById('toggle');
+const hamburger = document.getElementById('hamburger-menu');
 
 toggle.addEventListener('change', () => {
   if (toggle.checked) {
@@ -11,5 +12,14 @@ toggle.addEventListener('change', () => {
   }
 });
 
-      
-
+// Deteksi klik di luar navbar dan hamburger
+document.addEventListener('click', (event) => {
+  // Jika navbar sedang terbuka
+  if (toggle.checked) {
+    // Jika klik bukan di navbar dan bukan di hamburger
+    if (!navbar.contains(event.target) && !hamburger.contains(event.target)) {
+      navbar.classList.add('opacity-0');
+      navbar.classList.remove('opacity-100');
+    }
+  }
+});
